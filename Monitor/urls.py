d8bf793420ca3dc1b80cabfc933ec354c21c610f
w190510995 <190510995@qq.com>
2018-12-04 18:23:23 +0800

    init3

--- Monitor/urls.py
+++ Monitor/urls.py
@@ -3,22 +3,40 @@
 author: 'WangSheng'
 date: '2018/12/4 17:52'
 
-from django.conf.urls  import url
+from django.conf.urls import url, include
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
 from . import views
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('url', 'username', 'email', 'is_staff')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+
+
+
+
+
+
+
+
 
 urlpatterns=[
 
-    url('', views.index),
+    url(r'^index', views.index),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     # url(r'^indexPage',views.signin),
-
     # url(r'^signin', views.signin),
-    # url(r'^sup',views.anlysit),
-    # url(r'^one',views.anlysit2),
-    # url(r'^many', views.anlysit3),
-    # url(r'^febakCarPoints', views.febakCarPoints),
-    # url(r'^carNum', views.carNumber),
-    # url(r'^febakSuppiles', views.febakSupplies),
-    # url(r'^pointSup', views.supPointsTest),
-    # url(r'^xcheckSingn',views.checkSingin),
-
 ]
